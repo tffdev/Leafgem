79dd99758851817e05a7442fd38cb98609892ef3
Bree Newfile <spambrecert@gmail.com>
2018-09-05 16:03:22 -0400

    :bulb: Documented mouse api

--- src/leafgem/mouse/click.cr
+++ src/leafgem/mouse/click.cr
@@ -3,25 +3,30 @@ class Leafgem::Mouse::Click
   property event : SDL::Event::MouseButton
   property held : Bool
 
+  # TODO: Update `#held?` and `#pressed?` automatically
   def initialize(@event, @pressed = true)
     @position = Vec2.new @event.x, @event.y
     @held = !@pressed
     # @when_pressed = event.timestamp
   end
 
+  # Has the button press been held?
   def held?
     @held
   end
 
+  # Was the button pressed?
   def pressed?
     @pressed
   end
 
+  # Update `#held?` and `#pressed?`
   def update
     @pressed = false
     @held = !@pressed
   end
 
+  # Update with a new mouse event
   def update(@event, @pressed = false)
     @position = Vec2.new @event.x, @event.y
     @held = !@pressed
