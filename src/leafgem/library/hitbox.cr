b62cb8d747cfa1a239c4d9c01c7f4d8439f9740b
Brecert <11599528+Brecert@users.noreply.github.com>
2018-09-11 17:42:13 -0400

    :sparkles: hitbox now now has mouse helper methods

--- src/leafgem/library/hitbox.cr
+++ src/leafgem/library/hitbox.cr
@@ -1,16 +1,27 @@
-include "../objects/hitbox"
+require "../objects/hitbox"
 
 class Leafgem::Hitbox
-	# Macro to make it easier to check for the mouse
-	macro mouse_in?
-		point_in? Mouse.pos.x, Mouse.pos.y 
-	end
+  # TODO: Change to a macro
+  # Is the mouse in the hitbox?
+  def mouse_in?
+    point_in? Library::Mouse.pos.x, Library::Mouse.pos.y
+  end
 
-	macro pressed?
-		mouse_in? && Mouse.pressed?
-	end
+  # Did the hitbox get pressed?
+  def pressed?
+    if b = Library::Mouse.primary
+      mouse_in? && b.pressed?
+    else
+      false
+    end
+  end
 
-	macro held?
-		mouse_in? && Mouse.held? 
-	end
+  # Was the hitbox held?
+  def held?
+    if b = Library::Mouse.primary
+      mouse_in? && b.held?
+    else
+      false
+    end
+  end
 end
