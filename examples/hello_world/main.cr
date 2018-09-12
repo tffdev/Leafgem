705903079628bdb5ecefda5cfbbe4f286019bc4e
Unknown <danielbri3r@gmail.com>
2018-09-12 22:23:00 +0100

    :art: Small hello world example update

--- examples/hello_world/main.cr
+++ examples/hello_world/main.cr
@@ -3,12 +3,12 @@ include Leafgem::Library
 
 set_window("Hello world!", 320, 240, 3)
 
-class Preview < Leafgem::GameObject
+class MyObject < Leafgem::GameObject
   def draw
     draw_sprite("examples/hello_world/sprite.png", pos.x, pos.y)
   end
 end
 
-create_object(Preview, 10, 10)
+create_object(MyObject, 10, 10)
 
-Leafgem::Game.run
+game_run
